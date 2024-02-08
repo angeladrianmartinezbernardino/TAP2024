1d169b38c1b55a5848a46c22915345de0977fbe3
Ángel Adrián Martínez Bernardino <angeladrianmartinezbernardino@gmail.com>
2024-02-08 17:28:47 -0600

    Nuevo.

---
+++ src/main/java/com/example/tap2024/vistas/Calculadora.java
@@ -0,0 +1,16 @@
+package com.example.tap2024.vistas;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Calculadora extends Stage {
+    private Scene escena;
+    private VBox vContenedor;
+    private GridPane gdpTeclado;
+    private TextField txtPantalla;
+    private Button[] arBotones = new Button[16];
+}
